75a4d5dc39635eff65e4e241b5d39204e758f4c4
Jacob Kravits <kravitsjacob@gmail.com>
2021-10-23 14:00:33 -0600

    Minor PEP8 formatting

--- optimization.py
+++ optimization.py
@@ -9,7 +9,7 @@ from borg import *
 
 
 # Global Vars
-pathto_data = '/app_io'
+pathto_data = 'MOEOPF_io'
 pathto_generator_limits = os.path.join(pathto_data, 'Input', 'generator_limits.csv')
 pathto_cost_coef = os.path.join(pathto_data, 'Input', 'costs.csv')
 pathto_emit_coef = os.path.join(pathto_data, 'Input', 'emissions.csv')
@@ -18,7 +18,7 @@ pathto_runtime = os.path.join(pathto_data, 'Output', 'runtime.txt')
 pathto_results = os.path.join(pathto_data, 'Output', 'results.csv')
 
 
-def solvePowerFlow(*vars):
+def solve_power_flow(*vars):
     """
     Solve for the power at generator 1 such that the powerflow equations are satisfied
     @param vars: tuple: Activate Power for Generators 2-5 in MW
@@ -39,7 +39,7 @@ def solvePowerFlow(*vars):
     return gen_df, bus_df
 
 
-def getGenConstraint(df):
+def get_gen_constraint(df):
     """
     Get generator power constraint value
     @param df: DataFrame: Generator power outputs
@@ -50,13 +50,13 @@ def getGenConstraint(df):
     # Import Generation Lim
     lim = pd.read_csv(pathto_generator_limits, index_col=0)
     # Exceeds Maximum Capacity
-    cons = cons + float(sum(x for x in df['p_mw']/100 - lim['max'] if x > 0)) # Unit Power
+    cons = cons + float(sum(x for x in df['p_mw']/100 - lim['max'] if x > 0))  # Unit Power
     cons = cons + float(abs(sum(x for x in df['p_mw']/100 - lim['min'] if x < 0)))
     # Export
     return cons
 
 
-def getCost(df):
+def get_cost(df):
     """
     Get fuel cost of current set of generators
     @param df: DataFrame: Generator power outputs
@@ -75,7 +75,7 @@ def getCost(df):
     return obj
 
 
-def getEmission(df):
+def get_emission(df):
     """
     Get emission value of current set of generators
     @param df: DataFrame: Generator power outputs
@@ -95,7 +95,7 @@ def getEmission(df):
     return obj
 
 
-def getSystemVoltageViolation(df):
+def get_system_voltage_violation(df):
     """
     Get sum of system-wide voltage violations
     @param df: DataFrame: Bus voltages
@@ -117,13 +117,13 @@ def simulation(*vars):
     @param vars: Generator power levels for generators 2-5
     @return: floats: objective and constraint values
     """
-    gen_df, bus_df = solvePowerFlow(*vars)
+    gen_df, bus_df = solve_power_flow(*vars)
     # Compute Constraint
-    generation_con = getGenConstraint(gen_df)
+    generation_con = get_gen_constraint(gen_df)
     # Compute Objectives
-    cost_obj = getCost(gen_df)
-    emit_obj = getEmission(gen_df)
-    system_volt_obj = getSystemVoltageViolation(bus_df)
+    cost_obj = get_cost(gen_df)
+    emit_obj = get_emission(gen_df)
+    system_volt_obj = get_system_voltage_violation(bus_df)
     # Format Results
     objs = [cost_obj, emit_obj, system_volt_obj]
     cons = [generation_con]
@@ -136,14 +136,19 @@ def simulation(*vars):
 
 
 def main():
-    # Setup Optimization
+    # Problem setup
     Configuration.seed(1008)
     borg = Borg(numberOfVariables=5, numberOfObjectives=3, numberOfConstraints=1, function=simulation)
     borg.setBounds((5, 150), (5, 150), (5, 150), (5, 150), (5, 150))
     borg.setEpsilons(0.1, 0.0001, 0.0001)
     print('Success: Setup Optimization')
     # Run Optimization
-    result = borg.solve({'maxEvaluations': 20000, 'initialPopulationSize': 100, 'runtimeformat': 'borg', 'frequency': 1000, 'runtimefile': pathto_runtime })
+    result = borg.solve(
+        {'maxEvaluations': 20000,
+         'initialPopulationSize': 100,
+         'runtimeformat': 'borg',
+         'frequency': 1000, 'runtimefile': pathto_runtime}
+    )
     print('Success: Run Optimization')
     return 0
 
